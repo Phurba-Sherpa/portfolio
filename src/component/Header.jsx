6ca6ec9479fdf9debfa865da4940a8f9dec35864
Phurba_ <phurba.sherpa@wowfinstacknepal.com>
2023-09-24 16:04:34 +0545

    Portfolio: 1) Add Responsive nav 3) Make website responsive to mobile

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,26 +1,27 @@
 export default function Header() {
-  return (
-    <div className="container lg:flex lg:gap-24 !py-24 lg:mt-auto mt-48">
-      <section className="flex flex-col basis-1/2">
-        <h1 className="pb-8 pr-8 intro-head">
-          Hello there, I'm Phurba - an independent software developer living in
-          south Asia
-        </h1>
-        <p className="intro-desc">
-          For over 2 years I've had experience with software development, have
-          knowledge of backend as well as front end. Check out some of my work
-          below, find out more about me, and don't hesitate to say hello back!
-        </p>
-        <div className="mt-12 xl:mt-20">
-          <a
-            href="mailto:phurba1404@gmail.com"
-            className="py-6 transition duration-300 bg-secondary px-14 text-grayscale-500 hover:bg-secondary-500"
-          >
-            phurba1404@gmail.com
-          </a>
+    return (
+        <div className="container sm:flex lg:gap-24 !py-16 lg:mt-auto mt-48">
+            <section className="flex flex-col lg:basis-1/2 basis-3/4">
+                <h1 className="pb-8 pr-8 intro-head">
+                    Hello there, I'm Phurba - an independent software developer
+                    living in south Asia
+                </h1>
+                <p className="intro-desc">
+                    For over 2 years I've had experience with software
+                    development, have knowledge of backend as well as front end.
+                    Check out some of my work below, find out more about me, and
+                    don't hesitate to say hello back!
+                </p>
+                <div className="mt-12 xl:mt-20">
+                    <a
+                        href="mailto:phurba1404@gmail.com"
+                        className="py-6 text-2xl transition duration-300 md:text-3xl bg-secondary px-14 text-grayscale-500 hover:bg-secondary-500"
+                    >
+                        phurba1404@gmail.com
+                    </a>
+                </div>
+            </section>
+            <section className="lg:basis-1/2 basis-1/4"></section>
         </div>
-      </section>
-      <section className="basis-1/2"></section>
-    </div>
-  );
+    )
 }
--- src/layout/horizonal-nav/Nav.jsx
+++ src/layout/horizonal-nav/Nav.jsx
@@ -1,71 +1,60 @@
-const Nav = () => {
-  return (
-    <nav className="flex items-center justify-between p-16 h-[80px] w-full fixed bg-primary z-10">
-      <div className="flex items-center gap-4">
-        <a href="#home" className="p-4 font-medium bg-logo-col text-primary">
-          PS
-        </a>
-        <p className="font-semibold uppercase text-grayscale-400">
-          Phurba Sherpa
-        </p>
-      </div>
-          <div className="md:hidden">
-            <Menu size={32} />
-          </div>
-        <ul className='absolute top-0 left-0 w-full h-screen text-3xl font-normal uppercase bg-white md:text-2xl md:space-x-16 md:flex text-grayscale-500'>
-          {navList.map((nav) => (
-            <li key={nav.name} className="py-3">
-              <a href={nav.url} className="inline-block nav-link">
-                {nav.name}
-              </a>
-            </li>
-        <li>
-          <a href="#experience" className="nav-link">
-            Experience
-          </a>
-        </li>
-        <li>
-          <a href="#project" className="nav-link">
-            Project
-          </a>
-        </li>
-        <li>
-          <a href="#contact" className="nav-link">
-            Contact
-          </a>
-        </li>
-      </ul>
+import { useState } from "react"
+import { Menu, X } from "react-feather"
 
-        {/* mobile menu */}
-        <div className="flex md:hidden">
-          <button>
-            <Menu size={24} />
-          </button>
-        </div>
-      </section>
-        <ul className="flex flex-col bg-red-200">
-          <li>
-            <a href="#" className="block bg-green-200">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block">
-              Experience
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block">
-              Project
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block">
-              Contact
-            </a>
-          </li>
-        </ul>
-    </nav>
-  );
-};
-export default Nav;
+const Nav = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+    const navOpenClass = menuOpen ? "header nav-open" : "header"
+    return (
+        <header className={navOpenClass}>
+            <div className="flex items-center space-x-4 text-base md:text-2xl">
+                <a href="#home" className="logo">
+                    PS
+                </a>
+                <p className="font-semibold uppercase text-grayscale-400 whitespace-nowrap">
+                    Phurba Sherpa
+                </p>
+            </div>
+            <nav className="main-nav">
+                <ul className="nav-list">
+                    {navList.map((nav) => (
+                        <li key={nav.name}>
+                            <a href={nav.url} className="nav-link">
+                                {nav.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+            <button
+                className="btn-mob-nav"
+                onClick={() => setMenuOpen((prev) => !prev)}
+            >
+                {menuOpen ? (
+                    <X className="icon-mob-nav" />
+                ) : (
+                    <Menu className="icon-mob-nav" />
+                )}
+            </button>
+        </header>
+    )
+}
+
+const navList = [
+    {
+        name: "about",
+        url: "#about",
+    },
+    {
+        name: "experience",
+        url: "#experience",
+    },
+    {
+        name: "project",
+        url: "#project",
+    },
+    {
+        name: "contact",
+        url: "#contact",
+    },
+]
+export default Nav
